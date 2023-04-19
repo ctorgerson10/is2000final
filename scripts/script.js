dbb8a9eb55a4c314e955b2602a276a18ba8614e1
ctorgerson10 <ctorgerson10@gmail.com>
2023-04-19 13:37:37 -0600

    minor changer

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // this code is for the hamburger menu.
-const toggleButton = document.querySelector('.navbar-toggle');
-const navbarLinks = document.querySelector('.navbar-links');
+const toggleButton = document.querySelector('._navbar-toggle');
+const navbarLinks = document.querySelector('._navbar-links');
 
 toggleButton.addEventListener('click', () => {
     // toggles the "active" class to show/hide the nav links
